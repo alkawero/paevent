0d96181b0c35ae2600d30cf91217c5d03113e28a
alka <alqawero@gmail.com>
2019-11-05 10:51:45 +0700

    some litlechanges

--- application/views/admin/participant_list.php
+++ application/views/admin/participant_list.php
@@ -24,8 +24,8 @@ if ($_SESSION['logged_in'] !== true) {
 				<table id="example" class="table table-striped table-bordered" style="width:100%">
 					<thead>
 						<tr>
-							<th>Name</th>
 							<th>Email</th>
+							<th>Code</th>
 							<th>Type</th>
 							<th>Phone</th>
 							<th>Price</th>
@@ -35,62 +35,74 @@ if ($_SESSION['logged_in'] !== true) {
 						</tr>
 					</thead>
 					<tbody>
-					<?php foreach ($participants as $participant): ?>
-						<tr>
-							<td><?=$participant->name;?></td>
-							<td><?=$participant->email;?></td>
-							<td>
-							<?php 
-								if($participant->type==1){
-									echo "Umum";
-								}else if($participant->status==2){
-									echo "Profesional Pendidik";
-								}else if($participant->status==3){
-									echo "Orang Tua Siswa Pahoa";
-								}
-								?>
-						
-						</td>
-							<td><?=$participant->phone;?></td>
-							<td>							
-								<?="Rp. ".number_format($participant->price, 0);?>
-							</td>
-							<td>								
-								<?php 
-								if($participant->status==1){
-									echo "Waiting";
-								}else if($participant->status==2){
-									echo "Uploaded";
-								}else if($participant->status==3){
-									echo "Paid";
-								}else if($participant->status==4){
-									echo "Rejected";
-								}
-								?>
-							</td>
-							<td>
-							<?php 
-								if($participant->status==1){
-									echo "-";
-								}else {								
-								?>
-								<a target='blank' href="<?=base_url('images/').$participant->bukti_bayar;?>">link</a>
-								<?php 
-								}
-								?>
-							</td>
-							<td>
-							<button onClick="approve(<?=$participant->payment_id;?>)" type="button" class="btn btn-sm btn-success">Approve</button>
-							<button onClick="reject(<?=$participant->payment_id;?>)" type="button" class="btn btn-sm btn-danger">Reject</button>
-							<button onClick="resend(<?=$participant->payment_id;?>)" type="button" class="btn btn-sm btn-primary">Resend Ticket</button>
-							</td>
-						</tr>
-						<?php endforeach; ?>	
+						<?php foreach ($participants as $participant) : ?>
+							<tr>
+								<td><?= $participant->email; ?></td>
+								<td><?= $participant->registration_code; ?></td>
+								<td>
+									<?php
+										if ($participant->type == 1) {
+											echo "Umum";
+										} else if ($participant->status == 2) {
+											echo "Profesional Pendidik";
+										} else if ($participant->status == 3) {
+											echo "Orang Tua Siswa Pahoa";
+										}
+										?>
+
+								</td>
+								<td><?= $participant->phone; ?></td>
+								<td>
+									<?= "Rp. " . number_format($participant->price, 0); ?>
+								</td>
+								<td>
+									<?php
+										if ($participant->status == 1) {
+											echo "Waiting";
+										} else if ($participant->status == 2) {
+											echo "Uploaded";
+										} else if ($participant->status == 3) {
+											echo "Paid";
+										} else if ($participant->status == 4) {
+											echo "Rejected";
+										}
+										?>
+								</td>
+								<td>
+									<?php
+										if ($participant->status == 1) {
+											echo "-";
+										} else {
+											?>
+										<a target='blank' href="<?= base_url('images/') . $participant->bukti_bayar; ?>">link</a>
+									<?php
+										}
+										?>
+								</td>
+								<td>
+									<?php
+										if ($participant->status == 2) {
+											?>
+										<button onClick="approve(<?= $participant->payment_id; ?>)" type="button" class="btn btn-sm btn-success">Approve</button>
+										<button onClick="reject(<?= $participant->payment_id; ?>)" type="button" class="btn btn-sm btn-danger">Reject</button>
+									<?php
+										} else if ($participant->status == 3) {
+											?>
+
+										<button onClick="resend(<?= $participant->payment_id; ?>)" type="button" class="btn btn-sm btn-primary">Resend</button>
+
+									<?php
+										}
+										?>
+
+								</td>
+							</tr>
+						<?php endforeach; ?>
 					</tbody>
 					<tfoot>
 						<tr>
-							<th>Name</th>
 							<th>Email</th>
+							<th>Code</th>
 							<th>Type</th>
 							<th>Phone</th>
 							<th>Price</th>
@@ -117,7 +129,7 @@ if ($_SESSION['logged_in'] !== true) {
 	<?php $this->load->view("admin/_partials/scrolltop.php") ?>
 	<?php $this->load->view("admin/_partials/modal.php") ?>
 	<?php $this->load->view("admin/_partials/js.php") ?>
-	<script src="<?php echo base_url('js/participant_list.js') ?>"></script>	
+	<script src="<?php echo base_url('js/participant_list.js') ?>"></script>
 
 </body>
 
